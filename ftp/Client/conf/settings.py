91d85452c57f28087ff99478d16c956e5830262e
WZSWang <tingyuweilou@163.com>
2019-01-29 17:37:20 +0800

    需求分析

---
+++ ftp/Client/conf/settings.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Date: 2019/1/29
+
+
+'''
+ftp/
+    ├─ Client/
+    │   ├─ bin/
+    │   │   ├─ ftp_client.py
+    │   │   └─ __init__.py
+    │   ├─ conf/
+    │   │   ├─ settings.py
+    │   │   └─ __init__.py
+    │   ├─ core/
+    │   │   ├─ core.py
+    │   │   └─ __init__.py
+    │   ├─ db/
+    │   │   └─ __init__.py
+    │   └─ __init__.py
+    ├─ Server/
+    │   ├─ bin/
+    │   │   ├─ ftp_server.py
+    │   │   └─ __init__.py
+    │   ├─ conf/
+    │   │   ├─ settings.py
+    │   │   └─ __init__.py
+    │   ├─ core/
+    │   │   ├─ core.py
+    │   │   └─ __init__.py
+    │   ├─ db/
+    │   │   └─ __init__.py
+    │   ├─ home/
+    │   └─ __init__.py
+    └─ __init__.py
+'''
